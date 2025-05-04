49b6778c99d809b87e7f9605190a1f9ebb33c7d4
ajaya timilsina <ajayatimilsina1@gmail.com>
2025-05-05 01:23:48 +0900

    dockerfileadd

---
+++ main/static/main/js/main.js
@@ -0,0 +1,8 @@
+<script>
+const toggle = document.getElementById('menu-toggle');
+const nav = document.querySelector('.site-nav');
+
+toggle.addEventListener('click', () => {
+  nav.classList.toggle('active');
+});
+</script>
